abbf0a6032536598d567f4b4b5d26cdb265e432f
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-07 22:50:53 -0800

    play sound upon touch

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
+    
+    // Snippet from https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
+    func playSound(_ noteIndex: Int) {
+        guard let url = Bundle.main.url(forResource: "note\(noteIndex)", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        print(sender.tag)
+        if sender.tag == 1 {
+            playSound(sender.tag)
+        } else if sender.tag == 2 {
+            
+        }
         
     }
     
-  
-
+    
+    
 }
 
