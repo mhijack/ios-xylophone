2a08f67a23726621c8d2235a067576574734d8a0
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-07 23:39:34 -0800

    implement audioPlayer

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -9,49 +9,56 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    var player: AVAudioPlayer?
+    // Use this class for audio playback unless you are playing audio captured from a network stream or require very low I/O latency.
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // Snippet from https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
-    func playSound(_ noteIndex: Int) {
-        guard let url = Bundle.main.url(forResource: "note\(noteIndex)", withExtension: "wav") else { return }
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            
-            /* iOS 10 and earlier require the following line:
-             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-            
-            guard let player = player else { return }
-            
-            player.play()
-            
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
+    //    // Snippet from https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
+    //    func playSound(_ noteIndex: Int) {
+    //        guard let url = Bundle.main.url(forResource: "note\(noteIndex)", withExtension: "wav") else { return }
+    //
+    //        do {
+    //            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+    //            try AVAudioSession.sharedInstance().setActive(true)
+    //
+    //            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+    //            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+    //
+    //            /* iOS 10 and earlier require the following line:
+    //             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+    //
+    //            guard let player = player else { return }
+    //
+    //            player.play()
+    //
+    //        } catch let error {
+    //            print(error.localizedDescription)
+    //        }
+    //    }
     
     @IBAction func notePressed(_ sender: UIButton) {
         
         print(sender.tag)
-        if sender.tag == 1 {
-            playSound(sender.tag)
-        } else if sender.tag == 2 {
-            
-        }
+        playSound(sender.tag)
         
     }
     
-    
+    func playSound(_ noteIndex: Int) {
+        let soundURL = Bundle.main.url(forResource: "note\(noteIndex)", withExtension: "wav")
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
     
 }
 
